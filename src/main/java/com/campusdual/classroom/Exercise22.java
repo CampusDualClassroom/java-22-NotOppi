c856a4974c6b5788da957dffb122c57268411dd0
NotOppi <Filippofg@outlook.com>
2025-05-23 14:50:47 +0200

    Listo

--- src/main/java/com/campusdual/classroom/Exercise22.java
+++ src/main/java/com/campusdual/classroom/Exercise22.java
@@ -8,17 +8,38 @@ public class Exercise22 {
 
     public static List<String> createArrayList() {
 
+        ArrayList<String> arreglo = new ArrayList<>();
+        arreglo.add("GKFFD");
+        arreglo.add("TNANA");
+        arreglo.add("MPMSL");
+        arreglo.add("PSWME");
+        arreglo.add("LZMLF");
+        arreglo.add("JYEBV");
+        arreglo.add("YELNT");
+        arreglo.add("JSNKR");
+        arreglo.add("JFESF");
+        arreglo.add("TMJLL");
+        return arreglo;
+
     }
 
     public static void printElementsAndIndex(List<String> customList) {
 
+        for (String element : customList){
+            System.out.println("El elemento " + element + " se encuentra en la posicion: " + customList.indexOf(element));
+        }
+
     }
 
     public static boolean addElementToList(List<String> customList, String element) {
-
+        return customList.add(element);
     }
 
     public static void main(String[] args) {
+
+        List<String> arrayList = createArrayList();
+        arrayList.add("AAAAA");
+        printElementsAndIndex(arrayList);
 
     }
 
